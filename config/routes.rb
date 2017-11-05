adf7aacf96e5f9c933ac317b9b2abc0f0f191bbd
ivanviyachki <ivan.viyachki@abv.bg>
2017-11-05 22:49:46 +0200

    asddgdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   get '/posts/:id/link', to: 'posts#link'
-  post '/notes/api', to: 'posts#craet'
+  post '/notes/api', to: 'posts#create'
 
   root 'note#new'
 end
